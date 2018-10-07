0c9d6b6bc7d5ca28b62bd637ab4053569a03189b
Sean Connolly <sconnolly17@gmail.com>
2018-10-06 21:35:15 -0600

    Make Home stylable and add the smallest of backgrounds

---
+++ components/Home.styles.js
@@ -0,0 +1,10 @@
+import css from 'styled-jsx/css';
+
+const styles = css`
+  .root {
+    background-image: url('/static/wavy-background.png');
+    background-repeat: no-repeat;
+  }
+`;
+
+export default styles;
