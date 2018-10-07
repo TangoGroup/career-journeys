a805e866328af1b4d62045518bf639631f5caab3
Sean Connolly <sconnolly17@gmail.com>
2018-10-06 20:58:02 -0600

    Make skills more flexy

--- components/SkillsOverview.styles.js
+++ components/SkillsOverview.styles.js
@@ -4,10 +4,12 @@ const styles = css`
   .root {
     display: flex;
     align-items: stretch;
+    flex-wrap: wrap;
+    justify-content: center;
   }
 
   .root > :global(*) {
-    margin: 0 2.4rem;
+    margin: 0 2.4rem 1.6rem 2.4rem;
   }
 `;
 
