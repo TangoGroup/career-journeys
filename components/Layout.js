9536d74bb8ac350bd7faa834a57c1c115affbe17
Sean Connolly <sconnolly17@gmail.com>
2018-11-26 14:26:48 -0700

    Add page for QA Roles

--- components/Layout.js
+++ components/Layout.js
@@ -11,6 +11,7 @@ const Layout = (props) => {
         <div className="top-nav-links">
           <Link href="/"><a className={router.pathname === '/' ? 'active' : ''}>Home</a></Link>
           <Link href="/roles"><a className={router.pathname === '/roles' ? 'active' : ''}>Roles</a></Link>
+          <Link href="/qa-roles"><a className={router.pathname === '/qa-roles' ? 'active' : ''}>QA Roles</a></Link>
           <Link href="/skills"><a className={router.pathname === '/skills' ? 'active' : ''}>Skills</a></Link>
         </div>
       </div>
---
+++ pages/qa-roles.js
@@ -0,0 +1,14 @@
+import SideNavLayout from '../components/SideNavLayout';
+
+const Roles = () => (
+  <SideNavLayout>
+    <SideNavLayout.Nav>
+      <div>QA Role Navigation</div>
+    </SideNavLayout.Nav>
+    <SideNavLayout.Content>
+      <div>QA Roles go here</div>
+    </SideNavLayout.Content>
+  </SideNavLayout>
+);
+
+export default Roles;
