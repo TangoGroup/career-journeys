34047f089ef219ffe408d5f093be335beb65ba9e
Sean Connolly <sconnolly17@gmail.com>
2018-11-26 14:33:40 -0700

    Adding QA Roles

---
+++ data/qa-roles.js
@@ -0,0 +1,36 @@
+const QA_ROLES = {
+  ASSOCIATE_QA_ENGINEER: {
+    title: 'Associate QA Engineer',
+    description: 'A brief paragraph about what it means to be an Associate QA Engineer',
+  },
+  QA_ENGINEER: {
+    title: 'QA Engineer',
+    description: 'A brief paragraph about what it means to be a QA Engineer',
+  },
+  SENIOR_QA_ENGINEER: {
+    title: 'Senior QA Engineer',
+    description: 'A brief paragraph about what it means to be a Senior QA Engineer',
+  },
+  QA_AUTOMATION_ENGINEER: {
+    title: 'QA Automation Engineer',
+    description: 'A brief paragraph about what it means to be a QA Automation Engineer',
+  },
+  SENIOR_QA_AUTOMATION_ENGINEER: {
+    title: 'Senior QA Automation Engineer',
+    description: 'A brief paragraph about what it means to be a Seniot QA Automation Engineer',
+  },
+  QA_MANAGER: {
+    title: 'QA Manager',
+    description: 'A brief paragraph about what it means to be a QA Manager',
+  },
+  QA_DIRECTOR: {
+    title: 'QA Director',
+    description: 'A brief paragraph about what it means to be an QA Director',
+  },
+  STAFF_QA_AUTOMATION_ENGINEER: {
+    title: 'Staff QA Automation Engineer',
+    description: 'A brief paragraph about what it means to be a Staff QA Automation Engineer',
+  },
+};
+
+export default QA_ROLES;
