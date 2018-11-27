c5cebcbf8c054cda0c0a3b6e56ac02a32473c849
Sean Connolly <sconnolly17@gmail.com>
2018-11-27 05:33:04 -0700

    Adding tech excellence behaviors for QA

--- data/qa-expectations.js
+++ data/qa-expectations.js
@@ -2,10 +2,10 @@ const QA_ROLE_EXPECTATIONS = {
   ASSOCIATE_QA_ENGINEER: {
     TECHNICAL_EXCELLENCE: {
       behaviors: [
-        'Demonstrates broad knowledge of programming concepts.',
-        'Works effectively within established architectures, following current best practices.',
-        'Can solve small-sized problems, can understand and contribute to solving medium-sized problems.',
-        'Demonstrates basic understanding of testing frameworks.',
+        'Able to follow test cases and acceptance criteria when executing manual testing.',
+        'Support in the creation and maintenance of documentation (Test Cases, Process docs, Test Plans).',
+        'Troubleshoot defects and accurately pinpoint errors.',
+        'Understand basic web and mobile testing strategies for testing cross-browser and mobile apps.',
       ],
     },
     DELIVERY: {
@@ -24,10 +24,10 @@ const QA_ROLE_EXPECTATIONS = {
   QA_ENGINEER: {
     TECHNICAL_EXCELLENCE: {
       behaviors: [
-        'Owns a small-to-medium feature from technical design through completion.',
-        'Can solve medium-sized problems, can understand and contribute to solving large-sized problems.',
-        'Tests new code thoroughly, both locally, and in production once shipped.',
-        'Develops new instances of existing architecture, or minor improvements to existing architecture.',
+        'Perform exploratory, functional, regression, integration and end-to-end testing.',
+        'Able run automated tests and troubleshoot failures.',
+        'Able to test mobile apps against different versions and environments.',
+        'Able to test Web on multi-browsers while using DevTools proficiently to troubleshoot errors.',
       ],
     },
     DELIVERY: {
@@ -46,9 +46,10 @@ const QA_ROLE_EXPECTATIONS = {
   SENIOR_QA_ENGINEER: {
     TECHNICAL_EXCELLENCE: {
       behaviors: [
-        'Owns a service or large technology component.',
-        'Can solve large-sized problems with well designed and architected solutions.',
-        'Designs and implements testable solutions to problems. Consistently writes tests for new features and bug fixes. Adds tests for uncovered areas.',
+        'Assist  in architecting manual test suites and frameworks.',
+        'Maintain and create new automation test cases.',
+        'Able to test multiple clients (Android/iOS/web) and platform implementations.',
+        'Assist in managing test suites, team boards, and process documentation',
       ],
     },
     DELIVERY: {
@@ -67,10 +68,12 @@ const QA_ROLE_EXPECTATIONS = {
   QA_AUTOMATION_ENGINEER: {
     TECHNICAL_EXCELLENCE: {
       behaviors: [
-        'Owns large-scale and impactful service or collection of services.',
-        'Mentors peers to avoid antipatterns.',
-        'Builds complex, reusable architectures that demonstrate best practices and enable engineers to work more effectively.',
-        'Actively educates their team on testing best practices. Builds systems so as to eliminate entire classes of programmer error.',
+        'Knowledge of automation frameworks',
+        'Basic understanding of SQL',
+        'Basic understanding of API testing tools',
+        'Basic understanding of browser development tools (Chrome DevTools)',
+        'Basic understanding of mobile debugging tools (Charles)',
+        'Basic understanding of black-box, gray-box and white-box testing',
       ],
     },
     DELIVERY: {
@@ -89,10 +92,12 @@ const QA_ROLE_EXPECTATIONS = {
   SENIOR_QA_AUTOMATION_ENGINEER: {
     TECHNICAL_EXCELLENCE: {
       behaviors: [
-        'Designs and builds complex, flexible architectures that demonstrates best practices and prepares for future needs.',
-        'Demonstrates organization-level expertise in a particular area or go-to person for solving problems in their domain expertise for all of engineering.',
-        'Actively educates their team in their areas of knowledge.',
-        'Designs systems and applications for testability and quality.  Marshals resources to improve testability and quality of systems and applications that need it.',
+        'Knowledge of an OOP language (Swift,Java, C#, Objective-C, etc…)',
+        'Knowledge of a scripting language (python, javascript etc..)',
+        'Understanding of SQL',
+        'Understanding of API testing tools',
+        'Understanding of Automation Frameworks (selenium, cucumber etc…)',
+        'Understanding of Gherkin',
       ],
     },
     DELIVERY: {
@@ -111,10 +116,7 @@ const QA_ROLE_EXPECTATIONS = {
   STAFF_QA_AUTOMATION_ENGINEER: {
     TECHNICAL_EXCELLENCE: {
       behaviors: [
-        'Helps solve larger technical and architectural problems with the team when they need support.',
-        'Demonstrates solid understanding of entire system, understands the architecture, is able to contribute across multiple technologies, and presents other technologies to leverage.',
-        'Works directly with other organizations to help solve technical problems.',
-        'Holds entire team including Staff, Principal and Architects to technical expectations and agreements.',
+        'Understanding of automation architecture',
       ],
     },
     DELIVERY: {
