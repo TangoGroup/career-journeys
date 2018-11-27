24d9f6105dca9d94231b23cf5607a4875ad7b4ff
Sean Connolly <sconnolly17@gmail.com>
2018-11-27 05:39:31 -0700

    These descriptions arent used

--- components/Role.js
+++ components/Role.js
@@ -8,7 +8,6 @@ const Role = (props) => {
     <div className="root">
       <div id={role.key} className="hidden-anchor" />
       <h2>{role.title}</h2>
-      <p>{role.description}</p>
       {Object.keys(SKILLS).map(skillKey => (
         <RoleSkillExpectations key={skillKey} roleKey={role.key} skillKey={skillKey} />
       ))}
--- data/roles.js
+++ data/roles.js
@@ -1,35 +1,27 @@
 const ROLES = {
   ASSOCIATE_ENGINEER: {
     title: 'Associate Engineer',
-    description: 'A brief paragraph about what it means to be an Associate Engineer',
   },
   ENGINEER: {
     title: 'Engineer',
-    description: 'A brief paragraph about what it means to be an Engineer',
   },
   SENIOR_ENGINEER: {
     title: 'Senior Engineer',
-    description: 'A brief paragraph about what it means to be a Senior Engineer',
   },
   STAFF_ENGINEER: {
     title: 'Staff Engineer',
-    description: 'A brief paragraph about what it means to be a Staff Engineer',
   },
   SENIOR_STAFF_ENGINEER: {
     title: 'Senior Staff Engineer',
-    description: 'A brief paragraph about what it means to be a Senior Staff Engineer',
   },
   ENGINEERING_MANAGER: {
     title: 'Engineering Manager',
-    description: 'A brief paragraph about what it means to be an Engineering Manager',
   },
   PRINCIPAL_ENGINEER: {
     title: 'Principal Engineer',
-    description: 'A brief paragraph about what it means to be a Principal Engineer',
   },
   ENGINEERING_DIRECTOR: {
     title: 'Engineering Director',
-    description: 'A brief paragraph about what it means to be an Engineering Director',
   },
 };
 
